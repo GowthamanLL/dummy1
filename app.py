b259ede6aec9621f78e88c8c6769549d5c6e7ffa
gowthaman <gowthaman6382@gmail.com>
2023-09-06 17:06:53 +0530

    h

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-a=1
-<<<<<<< HEAD
-print(a)
-=======
-a=16
-=======
->>>>>>> parent of e015552 (a)
+
 b=20
 d=10
 c=10
 f=90
-print(a+b+c)
-<<<<<<< HEAD
->>>>>>> parent of cd8d7d2 (FF)
-=======
->>>>>>> parent of e015552 (a)
+print(b+c)
